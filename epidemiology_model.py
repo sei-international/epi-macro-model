d8b2f0a57933c0776b872864e236b0c4cb5afc92
nishunaz <54987037+nishunaz@users.noreply.github.com>
2021-01-27 08:39:24 -0500

    Add files via upload
    Rural/Urban - Pre encapsulation

--- epidemiology_model.py
+++ epidemiology_model.py
@@ -34,8 +34,8 @@ with open(r'common_params.yaml') as file:
     common_params = yaml.full_load(file)
 
 # Load epidemiological model object
-epi = SEIR_matrix(r'seir_params.yaml', common_params['initial'], common_params['geography']['number of localities'])
-
+epi_urban = SEIR_matrix(r'seir_params.yaml', common_params['initial'], common_params['geography'][0]['number of localities'],'urban')
+epi_rural = SEIR_matrix(r'seir_params.yaml', common_params['initial'], common_params['geography'][1]['number of localities'], 'rural')
 start_time = 0
 start_datetime = get_datetime(common_params['time']['start date'])
 end_time = (get_datetime(common_params['time']['end date']) - start_datetime).days
@@ -54,7 +54,7 @@ for window in common_params['isolate symptomatic cases']:
                                                         (get_datetime(window['end date']) - start_datetime).days,
                                                         window['ramp up for'],
                                                         window['ramp down for'],
-                                                        (1 - epi.invisible_fraction) * window['fraction of cases isolated']))
+                                                        (1 - epi_urban.invisible_fraction) * window['fraction of cases isolated']))
 
 test_and_trace_windows = []
 for window in common_params['test and trace']:
@@ -74,32 +74,57 @@ for window in common_params['social distance']:
                                        window['ramp down for'],
                                        window['effectiveness']))
 
-infected_arrivals = common_params['international travel']['daily arrivals'] * \
+infected_arrivals_urban = (common_params['international travel']['daily arrivals'] * \
                     common_params['international travel']['fraction infected'] * \
-                    common_params['international travel']['duration of stay']
-                    
-internal_movement_per_person = common_params['geography']['internal mobility rate']
+                    common_params['international travel']['duration of stay']) + \
+                          (common_params['geography'][1]['population'] * \
+                    common_params['geography'][1]['external mobility rate'])
+infected_arrivals_rural = (common_params['geography'][0]['population'] * \
+                    common_params['geography'][0]['external mobility rate'])
+internal_movement_per_person_urban = common_params['geography'][0]['internal mobility rate']
+internal_movement_per_person_rural = common_params['geography'][1]['internal mobility rate']
 
 
-# Initialize values for indicator graphs
-deaths = 0
-cumulative_cases = 0
-susceptible_over_time = np.zeros(end_time - start_time + 1)
-susceptible_over_time[0] = epi.S
-exposed_over_time = np.zeros(end_time - start_time + 1)
-exposed_over_time[0] = np.sum(epi.E)
-infective_over_time = np.zeros(end_time - start_time + 1)
-infective_over_time[0] = np.sum(epi.I_nr + epi.I_r)
-deaths_over_time = np.zeros(end_time - start_time + 1)
-deaths_over_time[0] = 0
-new_deaths_over_time = np.zeros(end_time - start_time + 1)
-new_deaths_over_time[0] = 0
-recovered_over_time = np.zeros(end_time - start_time + 1)
-recovered_over_time[0] = 0
-initial_population = epi.N
-comm_spread_frac_over_time = np.zeros(end_time - start_time + 1)
-comm_spread_frac_over_time[0] = epi.comm_spread_frac
-mortality_rate_over_time = np.zeros(end_time - start_time + 1)
+
+# Initialize values for indicator graphs urban
+deaths_urban = 0
+cumulative_cases_urban = 0
+susceptible_over_time_urban = np.zeros(end_time - start_time + 1)
+susceptible_over_time_urban[0] = epi_urban.S
+exposed_over_time_urban = np.zeros(end_time - start_time + 1)
+exposed_over_time_urban[0] = np.sum(epi_urban.E)
+infective_over_time_urban = np.zeros(end_time - start_time + 1)
+infective_over_time_urban[0] = np.sum(epi_urban.I_nr + epi_urban.I_r)
+deaths_over_time_urban = np.zeros(end_time - start_time + 1)
+deaths_over_time_urban[0] = 0
+new_deaths_over_time_urban = np.zeros(end_time - start_time + 1)
+new_deaths_over_time_urban[0] = 0
+recovered_over_time_urban = np.zeros(end_time - start_time + 1)
+recovered_over_time_urban[0] = 0
+initial_population_urban = epi_urban.N
+comm_spread_frac_over_time_urban = np.zeros(end_time - start_time + 1)
+comm_spread_frac_over_time_urban[0] = epi_urban.comm_spread_frac
+mortality_rate_over_time_urban = np.zeros(end_time - start_time + 1)
+
+# Initialize values for indicator graphs rural
+deaths_rural = 0
+cumulative_cases_rural = 0
+susceptible_over_time_rural = np.zeros(end_time - start_time + 1)
+susceptible_over_time_rural[0] = epi_urban.S
+exposed_over_time_rural = np.zeros(end_time - start_time + 1)
+exposed_over_time_rural[0] = np.sum(epi_urban.E)
+infective_over_time_rural = np.zeros(end_time - start_time + 1)
+infective_over_time_rural[0] = np.sum(epi_urban.I_nr + epi_urban.I_r)
+deaths_over_time_rural = np.zeros(end_time - start_time + 1)
+deaths_over_time_rural[0] = 0
+new_deaths_over_time_rural = np.zeros(end_time - start_time + 1)
+new_deaths_over_time_rural[0] = 0
+recovered_over_time_rural = np.zeros(end_time - start_time + 1)
+recovered_over_time_rural[0] = 0
+initial_population_rural = epi_urban.N
+comm_spread_frac_over_time_rural = np.zeros(end_time - start_time + 1)
+comm_spread_frac_over_time_rural[0] = epi_urban.comm_spread_frac
+mortality_rate_over_time_rural = np.zeros(end_time - start_time + 1)
 
 for i in range(start_time, end_time):
 
@@ -115,43 +140,57 @@ for i in range(start_time, end_time):
         PHA_isolate_infectious_cases += w.window(i)
     PHA_isolate_cases = max(PHA_isolate_visible_cases, PHA_isolate_infectious_cases)
     public_health_adjustment = (1 - PHA_social_distancing) * (1 - PHA_isolate_cases)
-    
+
     # Beds and Mortality
     if avoid_elective_operations:
         bed_occupancy_factor = (1 - PHA_social_distancing * max_reduction_in_normal_bed_occupancy)
     else:
         bed_occupancy_factor = 1
     bed_occupancy_fraction = bed_occupancy_factor * normal_bed_occupancy_fraction
-    
-    # Run the model for one time step
-    epi.update(infected_arrivals, internal_movement_per_person, public_health_adjustment, bed_occupancy_fraction, beds_per_1000)
-    
-    # Update values for indicator graphs
-    new_deaths_over_time[i] = epi.new_deaths
-    deaths += epi.new_deaths
-    cumulative_cases_per_1000 = 1000 * cumulative_cases/initial_population
-    susceptible_over_time[i] = epi.S
-    exposed_over_time[i] = np.sum(epi.E)
-    infective_over_time[i] = epi.Itot
-    deaths_over_time[i] = deaths
-    recovered_over_time[i] = epi.R
-    new_visible_cases = (1 - epi.invisible_fraction) * (epi.I_nr[1] + epi.I_r[1])
-    cumulative_cases += new_visible_cases
-    comm_spread_frac_over_time[i] = epi.comm_spread_frac
-    mortality_rate_over_time[i] = epi.curr_mortality_rate
 
-locator = mdates.AutoDateLocator()
-formatter = mdates.ConciseDateFormatter(locator)
+    # Run the model for one time step
+    epi_urban.update(infected_arrivals_urban, internal_movement_per_person_urban, public_health_adjustment, bed_occupancy_fraction, beds_per_1000)
+    epi_rural.update(infected_arrivals_rural, internal_movement_per_person_rural, public_health_adjustment, bed_occupancy_fraction, beds_per_1000)
+
+    # Update values for indicator graphs
+    new_deaths_over_time_urban[i] = epi_urban.new_deaths
+    deaths_urban += epi_urban.new_deaths
+    cumulative_cases_per_1000_urban = 1000 * cumulative_cases_urban/initial_population_urban
+    susceptible_over_time_urban[i] = epi_urban.S
+    exposed_over_time_urban[i] = np.sum(epi_urban.E)
+    infective_over_time_urban[i] = epi_urban.Itot
+    deaths_over_time_urban[i] = deaths_urban
+    recovered_over_time_urban[i] = epi_urban.R
+    new_visible_cases_urban = (1 - epi_urban.invisible_fraction) * (epi_urban.I_nr[1] + epi_urban.I_r[1])
+    cumulative_cases_urban += new_visible_cases_urban
+    comm_spread_frac_over_time_urban[i] = epi_urban.comm_spread_frac
+    mortality_rate_over_time_urban[i] = epi_urban.curr_mortality_rate
+
+    new_deaths_over_time_rural[i] = epi_rural.new_deaths
+    deaths_rural += epi_rural.new_deaths
+    cumulative_cases_per_1000_rural = 1000 * cumulative_cases_rural / initial_population_rural
+    susceptible_over_time_rural[i] = epi_rural.S
+    exposed_over_time_rural[i] = np.sum(epi_rural.E)
+    infective_over_time_rural[i] = epi_rural.Itot
+    deaths_over_time_rural[i] = deaths_rural
+    recovered_over_time_rural[i] = epi_rural.R
+    new_visible_cases_rural = (1 - epi_rural.invisible_fraction) * (epi_rural.I_nr[1] + epi_rural.I_r[1])
+    cumulative_cases_rural += new_visible_cases_rural
+    comm_spread_frac_over_time_rural[i] = epi_rural.comm_spread_frac
+    mortality_rate_over_time_rural[i] = epi_rural.curr_mortality_rate
+
+    locator = mdates.AutoDateLocator()
+    formatter = mdates.ConciseDateFormatter(locator)
 
 ax = plt.gca()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
 plt.stackplot(datetime_array,
-              susceptible_over_time[start_time:end_time],
-              exposed_over_time[start_time:end_time],
-              infective_over_time[start_time:end_time],
-              recovered_over_time[start_time:end_time],
-              deaths_over_time[start_time:end_time],
+              susceptible_over_time_urban[start_time:end_time],
+              exposed_over_time_urban[start_time:end_time],
+              infective_over_time_urban[start_time:end_time],
+              recovered_over_time_urban[start_time:end_time],
+              deaths_over_time_urban[start_time:end_time],
               labels=['susceptible','exposed','infected','recovered','died'])
 plt.legend(loc='lower right')
 plt.show()
@@ -159,28 +198,68 @@ plt.show()
 ax = plt.gca()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-plt.plot(datetime_array, deaths_over_time[start_time:end_time])
-plt.ylabel('cumulative deaths')
+plt.plot(datetime_array, deaths_over_time_urban[start_time:end_time])
+plt.ylabel('cumulative deaths urban')
 plt.show()
 
 ax = plt.gca()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-plt.plot(datetime_array, new_deaths_over_time[start_time:end_time])
-plt.ylabel('new deaths/day')
+plt.plot(datetime_array, new_deaths_over_time_urban[start_time:end_time])
+plt.ylabel('new deaths/day urban')
 plt.show()
 
 ax = plt.gca()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-plt.plot(datetime_array, comm_spread_frac_over_time[start_time:end_time])
-plt.ylabel('community spread')
+plt.plot(datetime_array, comm_spread_frac_over_time_urban[start_time:end_time])
+plt.ylabel('community spread urban')
 plt.show()
 
 ax = plt.gca()
 ax.xaxis.set_major_locator(locator)
 ax.xaxis.set_major_formatter(formatter)
-plt.plot(datetime_array, mortality_rate_over_time[start_time:end_time])
-plt.ylabel('mortality rate')
+plt.plot(datetime_array, mortality_rate_over_time_urban[start_time:end_time])
+plt.ylabel('mortality rate urban')
 plt.show()
 
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+plt.stackplot(datetime_array,
+              susceptible_over_time_rural[start_time:end_time],
+              exposed_over_time_rural[start_time:end_time],
+              infective_over_time_rural[start_time:end_time],
+              recovered_over_time_rural[start_time:end_time],
+              deaths_over_time_rural[start_time:end_time],
+              labels=['susceptible', 'exposed', 'infected', 'recovered', 'died'])
+plt.legend(loc='lower right')
+plt.show()
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+plt.plot(datetime_array, deaths_over_time_rural[start_time:end_time])
+plt.ylabel('cumulative deaths rural')
+plt.show()
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+plt.plot(datetime_array, new_deaths_over_time_rural[start_time:end_time])
+plt.ylabel('new deaths/day rural')
+plt.show()
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+plt.plot(datetime_array, comm_spread_frac_over_time_rural[start_time:end_time])
+plt.ylabel('community spread rural')
+plt.show()
+
+ax = plt.gca()
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+plt.plot(datetime_array, mortality_rate_over_time_rural[start_time:end_time])
+plt.ylabel('mortality rate rural')
+plt.show()
--- seir_model.py
+++ seir_model.py
@@ -3,7 +3,7 @@ from scipy import special as sp
 import yaml
 
 class SEIR_matrix:
-    def __init__(self, seir_params_file: str, initial_values: dict, n_loc: int):
+    def __init__(self, seir_params_file: str, initial_values: dict, n_loc: int, loc_type: str):
         """ Create a new SEIR_matrix object
 
         Parameters
@@ -93,7 +93,7 @@ class SEIR_matrix:
         # State variables
         #-------------------------------------------------------------------
         # Total population and population at risk
-        self.N = initial_values['total population']
+        self.N = initial_values['total population']*initial_values[loc_type]
         initial_infected = initial_values['infected fraction'] * self.N
         self.Itot = initial_infected
         self.Itot_prev = initial_infected
