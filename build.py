3c4144d586ba985657152646385367ddcf63b18e
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2021-03-04 11:15:03 -0500

    Renamed master file and added icon files

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ for d in ('build', 'dist'):
     if os.path.exists(d) and os.path.isdir(d):
         shutil.rmtree(d)
 
-os.system('pyinstaller --onefile covid_baselines.py')
+os.system('pyinstaller --onefile --icon=sei_icon-32x32.ico cemm.py')
 
 if os.path.exists('dist') and os.path.isdir('dist'):
     os.system('cp *.yaml dist/')
