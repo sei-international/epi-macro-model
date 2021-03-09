747136227ad1b4fc967b47852f2e51ba55194da2
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2021-03-09 10:57:06 -0500

    Update build.py

--- build.py
+++ build.py
@@ -5,7 +5,7 @@ for d in ('build', 'dist'):
     if os.path.exists(d) and os.path.isdir(d):
         shutil.rmtree(d)
 
-os.system('pyinstaller --onefile --icon=sei_icon-32x32.ico cemm.py')
+os.system('pyinstaller --onefile --icon=sei_icon-32x32.ico recovrs.py')
 
 if os.path.exists('dist') and os.path.isdir('dist'):
     os.system('cp *.yaml dist/')
