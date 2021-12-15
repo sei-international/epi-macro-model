1114cd2a7f5be455a0e893402473bea968f837a8
Eric Kemp-Benedict <eric.kemp-benedict@sei.org>
2021-12-15 15:43:30 -0500

    Updated build.py for new program name

--- build.py
+++ build.py
@@ -5,8 +5,8 @@ for d in ('build', 'dist'):
     if os.path.exists(d) and os.path.isdir(d):
         shutil.rmtree(d)
 
-os.system('pyinstaller --onefile --icon=sei_icon-32x32.ico recovrs.py')
+os.system('pyinstaller --onefile --icon=sei_icon-32x32.ico epi_macro_model.py')
 
 if os.path.exists('dist') and os.path.isdir('dist'):
-    os.system('cp *.yaml dist/')
-    os.system('cp input_output_data.csv dist/')
+    os.system('copy *.yaml dist')
+    os.system('copy input_output_data.csv dist')
